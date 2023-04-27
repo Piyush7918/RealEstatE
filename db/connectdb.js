7ce528fb169182a98bd066ff3951d68e521895f1
Piyush7918 <mcan1ca21027@itmuniversity.ac.in>
2023-04-27 21:50:15 +0530

    re

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const url = "mongodb://127.0.0.1:27017/RealEstatE"
 
- const live_Url ="mongodb+srv://chauhanpiyush560:piyush123@cluster0.6rkz1ig.mongodb.net/realestate?retryWrites=true&w=majority"
+ const live_Url ="mongodb+srv://chauhanpiyush560:piyush1999@cluster0.6rkz1ig.mongodb.net/RealEstaE?retryWrites=true&w=majority"
 
 const connectDB = ()=>{
     return mongoose.connect(live_Url)
