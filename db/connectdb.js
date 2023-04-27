8053af097d3708139094ad12644c008dcaa5bebb
Piyush7918 <mcan1ca21027@itmuniversity.ac.in>
2023-04-27 16:47:50 +0530

    n

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const url = "mongodb://127.0.0.1:27017/RealEstatE"
 
+ const live_Url ="mongodb+srv://chauhanpiyush560:piyush123@cluster0.6rkz1ig.mongodb.net/realestate?retryWrites=true&w=majority"
 
 const connectDB = ()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_Url)
 
     .then(()=>{
         console.log("Database Connected.....")
